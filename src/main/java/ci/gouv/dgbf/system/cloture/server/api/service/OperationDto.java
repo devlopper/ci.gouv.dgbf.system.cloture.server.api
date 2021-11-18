4f0ec5e95e2b5b2ee1304ccdef4d70486cf3cc42
Christian Yao Komenan <kycdev@gmail.com>
2021-11-18 16:44:31 +0000

    commit

--- src/main/java/ci/gouv/dgbf/system/cloture/server/api/service/OperationDto.java
+++ src/main/java/ci/gouv/dgbf/system/cloture/server/api/service/OperationDto.java
@@ -17,12 +17,15 @@ public interface OperationDto extends IdentifiableSystemScalarStringIdentifiable
 	
 	Long getStartDateNumberOfMillisecond();
 	OperationDto setStartDateNumberOfMillisecond(Long startDateNumberOfMillisecond);
+	String getStartDateString();
 	
 	Long getExecutionBeginDateNumberOfMillisecond();
 	OperationDto setExecutionBeginDateNumberOfMillisecond(Long executionBeginDateNumberOfMillisecond);
+	String getExecutionBeginDateString();
 	
 	Long getExecutionEndDateNumberOfMillisecond();
 	OperationDto setExecutionEndDateNumberOfMillisecond(Long executionEndDateNumberOfMillisecond);
+	String getExecutionEndDateString();
 	
 	OperationExecutionStatus getExecutionStatus();
 	OperationDto setExecutionStatus(OperationExecutionStatus executionStatus);
@@ -33,9 +36,12 @@ public interface OperationDto extends IdentifiableSystemScalarStringIdentifiable
 	String JSON_GROUP = "groupe";
 	String JSON_GROUP_IDENTIFIER = "groupe_identifiant";
 	String JSON_START_DATE_NUMBER_OF_MILLISECOND = "date_debut_nombre_milliseconde";
+	String JSON_START_DATE_STRING = "date_debut_chaine";
 	String JSON_PROCEDURE_NAME = "procedure_nom";
 	String JSON_TRIGGER = "declencheur";
 	String JSON_EXECUTION_BEGIN_DATE_NUMBER_OF_MILLISECOND = "execution_date_debut_nombre_milliseconde";
+	String JSON_EXECUTION_BEGIN_DATE_STRING = "execution_date_debut_chaine";
 	String JSON_EXECUTION_END_DATE_NUMBER_OF_MILLISECOND = "execution_date_fin_nombre_milliseconde";
+	String JSON_EXECUTION_END_DATE_STRING = "execution_date_fin_chaine";
 	String JSON_EXECUTION_STATUS = "execution_status";
 }
