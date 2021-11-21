07d62e03b00e2c7fcf0d95df3907fd6ffe6446ee
Christian Yao Komenan <kycdev@gmail.com>
2021-11-21 19:30:18 +0000

    commit

--- src/main/java/ci/gouv/dgbf/system/cloture/server/api/persistence/Parameters.java
+++ src/main/java/ci/gouv/dgbf/system/cloture/server/api/persistence/Parameters.java
@@ -5,4 +5,6 @@ public interface Parameters {
 	String OPERATION_GROUP_IDENTIFIER = "go_id";
 	String OPERATION_IDENTIFIER = "o_id";
 	String ACT_IDENTIFIER = "a_id";
+	String ACT_OPERATION_TYPE = "toa_id";
+	String SEARCH = "r";
 }
