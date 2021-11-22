736cdc426e9d76ce63fc900f37f0d70ec5122ca9
Christian Yao Komenan <kycdev@gmail.com>
2021-11-22 15:34:34 +0000

    commit

--- src/main/java/ci/gouv/dgbf/system/cloture/server/api/persistence/Act.java
+++ src/main/java/ci/gouv/dgbf/system/cloture/server/api/persistence/Act.java
@@ -1,17 +1,15 @@
 package ci.gouv.dgbf.system.cloture.server.api.persistence;
 
-import java.time.LocalDateTime;
-
 import org.cyk.utility.persistence.entity.IdentifiableSystemScalarStringIdentifiableBusinessStringNamable;
 
 public interface Act extends IdentifiableSystemScalarStringIdentifiableBusinessStringNamable {
 	
 	ActOperationType getOperationType();
 	Act setOperationType(ActOperationType operationType);
-	
+	/*
 	LocalDateTime getOperationDate();
 	Act setOperationDate(LocalDateTime date);
-	
+	*/
 	String getOperationDateString();
 	Act setOperationDateString(String string);
 	
