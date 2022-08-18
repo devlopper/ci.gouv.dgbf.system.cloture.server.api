054293b71cb9fe5651aaba28bc098ffc608e50ce
devlopper <kycdev@gmail.com>
2022-08-18 15:18:54 +0000

    commit

--- src/main/java/ci/gouv/dgbf/system/cloture/server/api/persistence/ActPersistence.java
+++ src/main/java/ci/gouv/dgbf/system/cloture/server/api/persistence/ActPersistence.java
@@ -1,11 +1,15 @@
 package ci.gouv.dgbf.system.cloture.server.api.persistence;
 
+import java.util.Collection;
+
 import javax.persistence.EntityManager;
 
 import org.cyk.utility.persistence.SpecificPersistence;
 
 public interface ActPersistence extends SpecificPersistence<Act> {
 
+	Collection<String> readIdentifiersWhereNotExistByOperationIdentifier(String operationIdentifier);
+	
 	void lock(String identifier,String lockType,String targetTable);
 	void lock(String identifier,String lockType,String targetTable,EntityManager entityManager);
 	
--- src/main/java/ci/gouv/dgbf/system/cloture/server/api/persistence/OperationActPersistence.java
+++ src/main/java/ci/gouv/dgbf/system/cloture/server/api/persistence/OperationActPersistence.java
@@ -1,7 +1,12 @@
 package ci.gouv.dgbf.system.cloture.server.api.persistence;
 
+import java.util.Collection;
+
 import org.cyk.utility.persistence.SpecificPersistence;
 
 public interface OperationActPersistence extends SpecificPersistence<OperationAct> {
 
+	Collection<OperationAct> readByOperationIdentifierByNotActsIdentifiers(String operationIdentifier,Collection<String> actsIdentifiers);
+	Collection<OperationAct> readByOperationIdentifier(String operationIdentifier);
+	
 }
